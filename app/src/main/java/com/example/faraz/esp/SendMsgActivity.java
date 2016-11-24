843b44bc0d7d52ad7d0e6f4fe283a590a4e06615
yahya alshomrani <yalshomrani0002@stu.kau.edu.sa>
2016-11-25 00:20:48 +0300

    complete send message function change background color for speech to text layout change background color for mic icon

--- app/src/main/java/com/example/faraz/esp/SendMsgActivity.java
+++ app/src/main/java/com/example/faraz/esp/SendMsgActivity.java
@@ -1,21 +1,69 @@
 package com.example.faraz.esp;
 
+import android.app.ProgressDialog;
+import android.content.Context;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
+import android.widget.EditText;
 import android.widget.Toast;
 
+import com.android.volley.Request;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.VolleyLog;
+import com.android.volley.toolbox.StringRequest;
+
+import java.io.UnsupportedEncodingException;
+import java.net.URLEncoder;
+
 import Location.LocationFinder;
+import connection.RequestQueueSingleton;
 
 public class SendMsgActivity extends AppCompatActivity {
-    private static final String TAG = IntroActivity.class.getSimpleName();
+    private static final String TAG = SendMsgActivity.class.getSimpleName();
     double longitude;
     double latitude;
+
+    private EditText message;
+    private ProgressDialog pDialog;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.send_message);
+
+        message = (EditText) findViewById(R.id.editText);
+
+        pDialog = new ProgressDialog(this);
+        pDialog.setMessage("Loading...");
+        pDialog.setCancelable(false);
+    }
+
+    public void send(View view){
+        if(!isNetworkAvailable()){
+            Toast.makeText(this,"You need Internet connection to Send.",Toast.LENGTH_SHORT).show();
+            return;
+        }
+
+        if(message.getText().toString().length() == 0){
+            Toast.makeText(this,"Please Enter your message",Toast.LENGTH_SHORT).show();
+            return;
+        }
+
+        try {
+            longitude = LocationFinder.getInstance().getLocation().getLongitude();
+            latitude = LocationFinder.getInstance().getLocation().getLatitude();
+        }catch(NullPointerException e){
+            Log.d(TAG, e.getMessage());
+            Toast.makeText(this,"Cannot find your location",Toast.LENGTH_SHORT).show();
+            return;
+        }
+
+        sendDataToServer(message.getText().toString(),1);
     }
 
     public void back(View view) {
@@ -25,9 +73,57 @@ public class SendMsgActivity extends AppCompatActivity {
     @Override
     protected void onStart() {
         super.onStart();
-//        longitude = LocationFinder.getInstance().getLocation().getLongitude();
-//        latitude = LocationFinder.getInstance().getLocation().getLatitude();
-//        Toast.makeText(this, "Longitude : "+String.valueOf(longitude)+" Latitude : "+String.valueOf(latitude),Toast.LENGTH_LONG).show();
+    }
+
+    private boolean isNetworkAvailable() {
+        ConnectivityManager connectivityManager
+                = (ConnectivityManager) getSystemService(Context.CONNECTIVITY_SERVICE);
+        NetworkInfo activeNetworkInfo = connectivityManager.getActiveNetworkInfo();
+        return activeNetworkInfo != null && activeNetworkInfo.isConnected();
+    }
+
+    private void sendDataToServer(String msg, final int op){
+        showpDialog();
+
+        String encodedMessage = null;
+        try {
+            encodedMessage = URLEncoder.encode(msg, "utf-8");
+        } catch (UnsupportedEncodingException e) {
+            e.printStackTrace();
+        }
+
+        String url = getString(R.string.server)+getString(R.string.path)+"?op="+op+"&msg="+encodedMessage+
+                "&lat="+latitude+"&lon="+longitude;
+
+        Log.d("URL",url);
+        StringRequest stringRequest = new StringRequest(Request.Method.GET, url,
+                new Response.Listener<String>() {
+                    @Override
+                    public void onResponse(String response) {
+                        Log.d("onResponse",response);
+                        hidepDialog();
+                    }
+                }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                VolleyLog.d(TAG, "Error: " + error.getMessage());
+                hidepDialog();
+            }
+        });
+
+        message.setText("");
+        // Add the request to the RequestQueue.
+        RequestQueueSingleton.getInstance().addToRequestQueue(stringRequest);
+    }
+
+    private void showpDialog() {
+        if (!pDialog.isShowing())
+            pDialog.show();
+    }
+
+    private void hidepDialog() {
+        if (pDialog.isShowing())
+            pDialog.dismiss();
     }
 }
 
