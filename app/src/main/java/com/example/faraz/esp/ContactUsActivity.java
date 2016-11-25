395e96536a528ff1d2709bbd0ac18e817f233795
yahya alshomrani <yalshomrani0002@stu.kau.edu.sa>
2016-11-25 23:40:10 +0300

    complete contact us layout add external link to facebook add external link to twitter

--- app/src/main/java/com/example/faraz/esp/ContactUsActivity.java
+++ app/src/main/java/com/example/faraz/esp/ContactUsActivity.java
@@ -1,6 +1,7 @@
 package com.example.faraz.esp;
 
 import android.content.Intent;
+import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
@@ -35,6 +36,18 @@ public class ContactUsActivity extends AppCompatActivity {
             Toast.makeText(this, "There are no email clients installed.", Toast.LENGTH_SHORT).show();
             Log.d(TAG,ex.getMessage());
         }
+    }
+
+    public void facebook(View view){
+        Uri uri = Uri.parse("https://www.facebook.com/MinistryOfHajj");
+        Intent intent = new Intent(Intent.ACTION_VIEW, uri);
+        startActivity(intent);
+    }
+
+    public void twitter(View view){
+        Uri uri = Uri.parse("https://twitter.com/HajMinistry");
+        Intent intent = new Intent(Intent.ACTION_VIEW, uri);
+        startActivity(intent);
     }
 }
 
