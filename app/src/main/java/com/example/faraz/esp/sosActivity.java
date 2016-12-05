cb594d081a36d9365e961313297ebe3000f2aa00
yahya alshomrani <yalshomrani0002@stu.kau.edu.sa>
2016-12-06 01:31:10 +0300

    add custom dialog for sos layout

--- app/src/main/java/com/example/faraz/esp/sosActivity.java
+++ app/src/main/java/com/example/faraz/esp/sosActivity.java
@@ -1,12 +1,14 @@
 package com.example.faraz.esp;
 
 import android.app.Activity;
+import android.app.AlertDialog;
 import android.app.ProgressDialog;
 import android.content.Context;
 import android.net.ConnectivityManager;
 import android.net.NetworkInfo;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.widget.Toast;
 
@@ -26,6 +28,14 @@ public class SOSActivity extends Activity{
     double longitude;
 
     private ProgressDialog pDialog;
+    private AlertDialog.Builder noInternet_adb;
+    private AlertDialog.Builder noText_adb;
+    private AlertDialog.Builder failed_adb;
+    private AlertDialog.Builder succeed_adb;
+    private AlertDialog noInternet_ad;
+    private AlertDialog noText_ad;
+    private AlertDialog failed_ad;
+    private AlertDialog succeed_ad;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -33,13 +43,87 @@ public class SOSActivity extends Activity{
         setContentView(R.layout.sos_layout);
 
         pDialog = new ProgressDialog(this);
-        pDialog.setMessage("Loading...");
+        pDialog.setMessage("Sending...");
         pDialog.setCancelable(false);
+
+        noInternetDialog();
+        noTextDialog();
+        failedDialog();
+        succeedDialog();
+    }
+
+    private void noInternetDialog(){
+        noInternet_adb = new AlertDialog.Builder(this);
+        noInternet_adb.setMessage("SOS Request");
+
+        //you can use db.setView(R.layout.nointernet_dialog) but it requires 21 api and above,
+        //this app minimum api is 18
+        LayoutInflater inflater = (LayoutInflater)this.getSystemService (Context.LAYOUT_INFLATER_SERVICE);
+        View v = inflater.inflate(R.layout.nointernet_dialog, null);
+
+        noInternet_adb.setView(v);
+
+        noInternet_adb.setCancelable(false);
+        noInternet_adb.setPositiveButton("OK", null);
+
+        noInternet_ad = noInternet_adb.create();
+    }
+
+    private void noTextDialog(){
+        noText_adb = new AlertDialog.Builder(this);
+        noText_adb.setMessage("SOS Request");
+
+        //you can use db.setView(R.layout.nointernet_dialog) but it requires 21 api and above,
+        //this app minimum api is 18
+        LayoutInflater inflater = (LayoutInflater)this.getSystemService (Context.LAYOUT_INFLATER_SERVICE);
+        View v = inflater.inflate(R.layout.notext_dialog, null);
+
+        noText_adb.setView(v);
+
+        noText_adb.setCancelable(false);
+        noText_adb.setPositiveButton("OK", null);
+
+        noText_ad = noText_adb.create();
+    }
+
+    private void failedDialog(){
+        failed_adb = new AlertDialog.Builder(this);
+        failed_adb.setMessage("SOS Request");
+
+        //you can use db.setView(R.layout.nointernet_dialog) but it requires 21 api and above,
+        //this app minimum api is 18
+        LayoutInflater inflater = (LayoutInflater)this.getSystemService (Context.LAYOUT_INFLATER_SERVICE);
+        View v = inflater.inflate(R.layout.failed_dialog, null);
+
+        failed_adb.setView(v);
+
+        failed_adb.setCancelable(false);
+        failed_adb.setPositiveButton("OK", null);
+
+        failed_ad = failed_adb.create();
+    }
+
+    private void succeedDialog(){
+        succeed_adb = new AlertDialog.Builder(this);
+        succeed_adb.setMessage("SOS Request");
+
+        //you can use db.setView(R.layout.nointernet_dialog) but it requires 21 api and above,
+        //this app minimum api is 18
+        LayoutInflater inflater = (LayoutInflater)this.getSystemService (Context.LAYOUT_INFLATER_SERVICE);
+        View v = inflater.inflate(R.layout.succeed_dialog, null);
+
+        succeed_adb.setView(v);
+
+        succeed_adb.setCancelable(false);
+        succeed_adb.setPositiveButton("OK", null);
+
+        succeed_ad = succeed_adb.create();
     }
 
     public void send(View view){
         if(!isNetworkAvailable()){
-            Toast.makeText(this,"You need Internet connection to Send.",Toast.LENGTH_SHORT).show();
+            //Toast.makeText(this,"You need Internet connection to Send.",Toast.LENGTH_SHORT).show();
+            noInternet_ad.show();
             return;
         }
 
@@ -74,12 +158,14 @@ public class SOSActivity extends Activity{
                     public void onResponse(String response) {
                         Log.d("onResponse",response);
                         hidepDialog();
+                        succeed_ad.show();
                     }
                 }, new Response.ErrorListener() {
             @Override
             public void onErrorResponse(VolleyError error) {
                 VolleyLog.d(TAG, "Error: " + error.getMessage());
                 hidepDialog();
+                failed_ad.show();
             }
         });
 
@@ -96,4 +182,5 @@ public class SOSActivity extends Activity{
         if (pDialog.isShowing())
             pDialog.dismiss();
     }
+
 }
