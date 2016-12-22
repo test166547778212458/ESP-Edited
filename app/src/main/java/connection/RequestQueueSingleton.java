8276cc8851847529be8c7926ee99478bbcdf4867
yahya alshomrani <yalshomrani0002@stu.kau.edu.sa>
2016-12-23 01:41:49 +0300

    make RequestQueueSingleton service

--- app/src/main/java/connection/RequestQueueSingleton.java
+++ app/src/main/java/connection/RequestQueueSingleton.java
@@ -1,39 +1,42 @@
 package connection;
 
+import android.app.Service;
+import android.content.Intent;
+import android.os.Binder;
+import android.os.IBinder;
+import android.support.annotation.Nullable;
 import android.util.Log;
 
 import com.android.volley.Request;
 import com.android.volley.RequestQueue;
 import com.android.volley.toolbox.Volley;
 
-import Location.LocationFinder;
 
-
-public class RequestQueueSingleton{
+public class RequestQueueSingleton extends Service {
 
     private static final String TAG = RequestQueueSingleton.class.getSimpleName();
 
     private RequestQueue requestQueue;
 
-    private static RequestQueueSingleton Instance;
+    IBinder mBinder = new LocalBinder();
 
-
-    private RequestQueueSingleton() {
-        Instance = this;
+    @Nullable
+    @Override
+    public IBinder onBind(Intent intent) {
+        Log.e(TAG,"onBind");
+        return mBinder;
     }
 
-    public static synchronized RequestQueueSingleton getInstance() {
-        if(Instance == null){
-            Instance = new RequestQueueSingleton();
+    public class LocalBinder extends Binder {
+        public RequestQueueSingleton getServerInstance() {
+            return RequestQueueSingleton.this;
         }
-        return Instance;
     }
 
-    private RequestQueue getRequestQueue() {
+    public RequestQueue getRequestQueue() {
         if (requestQueue == null) {
-            requestQueue = Volley.newRequestQueue(LocationFinder.getInstance().getApp());
+            requestQueue = Volley.newRequestQueue(getApplicationContext());
         }
-        Log.d(TAG,requestQueue.toString());
         return requestQueue;
     }
 
@@ -41,5 +44,4 @@ public class RequestQueueSingleton{
         req.setTag(TAG);
         getRequestQueue().add(req);
     }
-
 }
